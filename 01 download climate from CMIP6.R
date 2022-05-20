e8adbfd672259c5d8727527290ad1a818eb3937b
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-05-20 08:02:41 -0500

    Download

--- 01 download climate from CMIP6.R
+++ 01 download climate from CMIP6.R
@@ -1,7 +1,13 @@
 
 
-
-
-
 # Load libraries ----------------------------------------------------------
 require(pacman)
+pacman::p_load(geodata, rgeos, gtools, dismo, sf, tidyverse, fs, corrplot)
+
+g <- gc(reset = TRUE)
+rm(list = ls())
+
+
+# To start ----------------------------------------------------------------
+
+
