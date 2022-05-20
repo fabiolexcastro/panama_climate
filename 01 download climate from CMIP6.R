c65c532fee72c5a2669668f9161957010aa9a9e6
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-05-20 08:07:29 -0500

    Download climate

--- 01 download climate from CMIP6.R
+++ 01 download climate from CMIP6.R
@@ -9,6 +9,11 @@ rm(list = ls())
 
 
 # To start ----------------------------------------------------------------
-ssps <- c('', '')
-prdo <- c('')
+ssps <- c('ssp126', 'ssp585')
+prdo <- c('2021-2040', '2041-2060', '2061-2080')
+mdls <- c('FIO-ESM', 'MPI-ESM1-2-HR', 'MPI-ESM1-2-LR')
 
+
+base <- 'https://geodata.ucdavis.edu/cmip6/tiles/'
+
+print(ssps); print(prdo); print(mdls); print(base)
