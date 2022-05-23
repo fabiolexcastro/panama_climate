fd48de34d4edd9e718d7c6ae17933b0169ac3bf3
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-05-23 09:22:18 -0500

    to make gwr

--- 02 processing climate.R
+++ 02 processing climate.R
@@ -6,6 +6,10 @@ pacman::p_load(geodata, spatialEco, rgeos, gtools, dismo, sf, tidyverse, fs, cor
 g <- gc(reset = TRUE)
 rm(list = ls())
 
+# Environmental SAGA ------------------------------------------------------
+envr <- 'D:/Cárdenas-Castillero/Trabajo/MiAmbiente/Shape and info/saga-8.2.0_x64/saga-8.2.0_x64'
+envr <- rsaga.env(envr)
+
 # Load data ---------------------------------------------------------------
 
 # Topography information
@@ -24,7 +28,17 @@ hlls <- terra::project(hlls, 'epsg:4326')
 terra::writeRaster(x = srtm, filename = '../raster/topography/srtm.tif')
 terra::writeRaster(x = hlls, filename = '../raster/topography/hlls.tif')
 
-# Climate information 
-library(spatialEco)
+# Climate data
+root <- '../raster/future/cm6/panama/'
+ssps <- c('ssp126', 'ssp585')
+mdls <- c('FIO-ESM-2-0', 'MPI-ESM1-2-HR', 'MPI-ESM1-2-LR')
+prds <- c('2021-2040', '2041-2060', '2061-2080')
+vars <- c('prec', 'tmax', 'tmin')
+
+# Convert stack to each one layer -----------------------------------------
+
+
+
+
 
 
