383c8a180dbc44490ece272e9a3231b4deb00503
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-05-23 09:09:59 -0500

    to make gwr

--- 02 processing climate.R
+++ 02 processing climate.R
@@ -9,9 +9,16 @@ rm(list = ls())
 # Load data ---------------------------------------------------------------
 
 # Topography information
-srtm <- '../raster/topography/pan/DEM_SAGA.tif'
-hlls <- '../raster/topography/pan/Hillshade_SRTM30m_214.tif'
+srtm <- '../raster/topograhy/pan/DEM_SAGA.tif'
+hlls <- '../raster/topograhy/pan/Hillshade_SRTM30m_214.tif'
 
 plot(terra::rast(srtm))
 plot(terra::rast(hlls))
+
+srtm <- terra::rast(srtm)
+srtm <- terra::project(srtm, 'epsg:4326')
+
+# Climate information 
+
+
 
