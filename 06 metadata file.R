86ffbf5eb57d42848c8c989da676c5e569c1daab
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-06-21 09:14:08 -0500

    Add the metadata file

---
+++ 06 metadata file.R
@@ -0,0 +1,25 @@
+
+
+
+# Load libraries ----------------------------------------------------------
+require(pacman)
+pacman::p_load(geodata, spatialEco, rgeos, gtools, dismo, sf, tidyverse, fs, corrplot)
+
+g <- gc(reset = TRUE)
+rm(list = ls())
+
+
+# Load data ---------------------------------------------------------------
+path <- 'G:/CLIMATECHANGE/Modelos'
+prds <- c('2030', '2050', '2070')
+mdls <- c('FIO-ESM2-0', 'MPI-ESM1-2-HR', 'MPI-ESM1-2-LR')
+ssps <- c('ssp126', 'ssp585')
+vars <- c('Precipitacion', 'Tmaxima', 'Tminima')
+znes <- c('Arco Seco', 'Caribe Occidental', 'Caribe Oriental', 
+          'Pacífico Occidental', 'Pacífico Oriental', 'Región Central')
+
+
+
+
+
+
