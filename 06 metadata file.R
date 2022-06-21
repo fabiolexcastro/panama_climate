eb62f07c342081d391cd080a895fb4bb953446e2
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-06-21 09:38:22 -0500

    upd

--- 06 metadata file.R
+++ 06 metadata file.R
@@ -54,10 +54,12 @@ addm <- function(prd, mdl, ssp){
       fl <- fl[-grep('vat', fl, value = F)]
       fl <- as.character(fl)
       fl <- fl[-grep('info', fl, value = F)]
-      fl <- fl[-grep('prec30_as', fl, value = F)]
+      fl <- fl[-grep('prec30_as$', fl, value = F)]
       fl
       
-      
+      rs <- terra::rast(fl)
+      plot(rs[[1]])
+      plot(rs[[14]])
       
       
     })
