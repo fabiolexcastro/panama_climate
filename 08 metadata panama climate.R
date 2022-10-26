12f3773f0532385305ac449172482424d6298dc2
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-10-26 17:11:17 -0500

    update

--- 08 metadata panama climate.R
+++ 08 metadata panama climate.R
@@ -9,6 +9,12 @@ rm(list = ls())
 options(scipen = 999, warn = -1)
 
 # Load data ---------------------------------------------------------------
-
 path <- 'G:/ProyectoR/raster/future/cm6/panama'
-dir_ls(path)
+ssps <- basename(dir_ls(path))
+mdls <- c('FIO-ESM-2-0', 'MPI-ESM1-2-HR', 'MPI-ESM1-2-LR')
+prds <- c('2021-2040', '2041-2060', '2061-2080')
+vars <- c('prec', 'tmax', 'tmin')
+
+# Function ----------------------------------------------------------------
+
+
