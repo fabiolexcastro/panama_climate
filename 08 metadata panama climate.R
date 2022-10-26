dc7035e435453e0919761a34e982d2d349b55520
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-10-26 17:07:52 -0500

    update

---
+++ 08 metadata panama climate.R
@@ -0,0 +1,14 @@
+
+
+# Load libraries ----------------------------------------------------------
+require(pacman)
+pacman::p_load(tidyverse, glue, fs, readxl, hablar)
+
+g <- gc(reset = TRUE)
+rm(list = ls())
+options(scipen = 999, warn = -1)
+
+# Load data ---------------------------------------------------------------
+
+path <- 'G:/ProyectoR/raster/future/cm6/panama'
+dir_ls(path)
